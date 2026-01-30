b53cdbad82dbd1bb4aae597253a63f69619ef1b1
Balakraj chaudhary <balakraj23@gmail.com>
2026-01-30 23:06:43 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://own-ai-virtual-assistant-2.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
